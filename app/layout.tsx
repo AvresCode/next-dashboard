bb73e7e6275f836c6a4600b0ca5a82810e5b15d4
AvresCode <servas230@gmail.com>
2024-09-28 20:17:46 +0200

    add font and image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 // import '@/app/ui/global.css'; // absolute import
 import './ui/global.css'; // relative import
+import { inter } from '@/app/ui/fonts';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
